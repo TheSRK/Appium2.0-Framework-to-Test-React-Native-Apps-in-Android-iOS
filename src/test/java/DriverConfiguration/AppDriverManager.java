21087bbd0427b407a47f2784580182ab5951d5e8
TheSRK <shargo3554@gmail.com>
2023-08-23 21:41:10 +0800

    update yml with google drive download option

--- src/test/java/DriverConfiguration/AppDriverManager.java
+++ src/test/java/DriverConfiguration/AppDriverManager.java
@@ -10,7 +10,7 @@ public class AppDriverManager {
     public static AppiumDriver appiumDriver=null;
     public static void initDriver() throws MalformedURLException {
         if (appiumDriver == null){
-            AppFactory.launchApp("ios", appiumDriver);
+            AppFactory.launchApp("android", appiumDriver);
         }
 
     }
